bbbcf5ab708aed0a672624c65d3f4b8c6436e1c9
Nikitina Victoria <vikitina@gmail.com>
2018-04-13 11:46:25 +0300

    main, radiobtns

--- mt/assets/js/script_new_wallet.js
+++ mt/assets/js/script_new_wallet.js
@@ -25,3 +25,30 @@ $(function () {
         zIndex: 999,
     });
 });
+
+$(function () {
+    $('.radio_group .toggle_radio').click(function(){
+
+    	if ($(this).hasClass('checked')){
+
+              $(this).removeClass('checked');
+
+    	}else{
+             $(this).parent().find('.toggle_radio').removeClass('checked');
+             $(this).parent().find('input').val($(this).data('value'));
+             $(this).addClass('checked');
+    	}
+    });
+
+	});
+
+/*
+                                         <div class="radio_group">
+                                             <input type="hidden" />
+                                             <span class="toggle_radio checked" data-value="idex">Idex</span>
+                                             <span class="toggle_radio" data-value="latoken">Latoken</span>
+                                             <span class="toggle_radio" data-value="etherdelta">EtherDelta</span>
+                                           </div><!-- radio_group  -->
+
+
+*/
