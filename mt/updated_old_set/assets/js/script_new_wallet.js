640a352d9a670f933691bd09aa02dfacf2d37450
Nikitina Victoria <vikitina@gmail.com>
2018-06-29 18:20:09 +0300

    notice of payment

---
+++ mt/updated_old_set/assets/js/script_new_wallet.js
@@ -0,0 +1,265 @@
+$(function(){
+	$('header .white_line .right_block_container .persona').click(function(){
+
+	            if($(this).hasClass('persona_open')){
+
+	                    $(this).removeClass('persona_open');
+	            }else{
+	                    $(this).addClass('persona_open');
+	            }
+	});
+	
+		$('header .persona_mob').click(function(){
+
+	            if($(this).hasClass('persona_mob_open')){
+
+	                    $(this).removeClass('persona_mob_open');
+	            }else{
+	                    $(this).addClass('persona_mob_open');
+	            }
+	});
+	$('header .blue_line .nav_mob').click(function(){
+		if($(this).hasClass('nav_mob_opened')){
+			$(this).removeClass('nav_mob_opened');
+			$('body').removeClass('fixed');
+		}else{
+            $(this).addClass('nav_mob_opened');
+            $('body').addClass('fixed');
+		}
+	});
+});
+
+$(function () {
+    $('[data-toggle="datepicker"]').datepicker({
+        autoHide: true,
+        autoPick: true,
+        format: 'dd.mm.yyyy',
+        zIndex: 999,
+    });
+});
+
+$(function () {
+    $('.radio_group .toggle_radio').click(function(){
+
+    	if ($(this).hasClass('checked')){
+
+              $(this).removeClass('checked');
+
+    	}else{
+             $(this).parent().find('.toggle_radio').removeClass('checked');
+             $(this).parent().find('input').val($(this).data('value'));
+             $(this).addClass('checked');
+    	}
+    });
+
+	});
+
+$(function () {
+    $('.inner_menu_trigger i').click(function(){
+
+    	if ($(this).parent().hasClass('opened')){
+
+              $(this).parent().removeClass('opened');
+
+    	}else{
+             $(this).parent().addClass('opened');
+    	}
+    });
+
+	});
+$(function(){
+
+	$('.checkbox_label').mouseup(function(){
+    
+		if($(this).find('.checkbox').hasClass('checkbox_checked')){
+             $(this).find('.checkbox').removeClass('checkbox_checked');
+             $(this).find('.checkbox input').val('');
+		}else{
+             $(this).find('.checkbox').addClass('checkbox_checked');
+             $(this).find('.checkbox input').val('1');
+             $(this).parent().removeClass('agree_error');
+             $('#unchecked_tick').removeClass('active');
+		}
+	});
+});
+
+$(function(){
+
+
+	$('.input-group-btn li a').click(function(){
+
+           $(this).parents('.input-group-btn').find('button').text($(this).text());
+           
+           if ($(this).parents('.input-group-btn').hasClass('only_button_input-group-btn')){
+               
+                   var link = $(this).data('link');
+                   var url = $(this).data('url');
+                   $(link).attr('href',url);
+
+                   
+
+           }
+	});
+
+    $('.input-group-btn li span').click(function(){
+
+           $(this).parents('.input-group-btn').find('button').text($(this).text());
+           
+           if ($(this).parents('.input-group-btn').hasClass('only_button_input-group-btn')){
+               
+                   var link = $(this).data('link');
+                   var url = $(this).data('url');
+                   $(link).attr('href',url);
+
+                   
+
+           }
+    });
+                                                  
+});
+
+
+$(function(){
+
+        $('.form form .expert_feature span').click(function(){
+
+        	if($(this).parent().hasClass('expert_feature_opened')){
+        		$(this).parent().removeClass('expert_feature_opened')
+        	}else{
+        		$(this).parent().addClass('expert_feature_opened')
+        	}
+        });
+});
+$(function(){
+
+    $('.wallet_block .w_block .list_of_currencies li').click(function(){
+
+        if($(this).hasClass('active')){
+            $(this).removeClass('active');
+
+        }else{
+            $('.wallet_block .w_block .list_of_currencies li').removeClass('active');
+            $(this).addClass('active');
+        }
+    });
+});
+
+$(function(){
+
+
+    $(document).mouseup(function (e){ 
+    var target_1 = $(".inner_menu_trigger");
+    var target_2 = $(".persona");
+    var target_3 = $(".wallet_block .w_block .list_of_currencies li");
+
+    if (!target_1.is(e.target)) { 
+       $(target_1).removeClass('opened');
+    } 
+    if (!target_2.is(e.target)) { 
+       $(target_2).removeClass('persona_open');
+    }     
+    if (!target_3.is(e.target)) { 
+       $(target_3).removeClass('active');
+    }      
+   
+ 
+  });
+});
+$(function(){
+
+    var h = $(window).height()-150;
+    $('#main_screen').css('min-height',h+'px');
+
+});
+
+$(function(){
+
+$('.lang').click(function(){
+
+  if ($(this).hasClass('opened_lang')) {
+    $(this).removeClass('opened_lang');
+  }else{
+
+    $(this).addClass('opened_lang');
+  }
+});
+
+});
+
+
+$(function(){
+
+    $('header .big_menu_btn').click(function(){
+       
+        $('.big_menu_block').addClass('big_menu_block_opened');
+        $('body').addClass('fixed');
+      
+    });
+});
+$(function(){
+
+    $('.big_menu_block .big_menu_content em').click(function(){
+
+        $('.big_menu_block').removeClass('big_menu_block_opened');
+        $('body').removeClass('fixed');
+        
+    });
+});
+
+$(function(){
+
+    $('.big_menu_block .big_menu_btn').click(function(){
+
+        $('.big_menu_block').removeClass('big_menu_block_opened');
+        $('body').removeClass('fixed');
+        
+    });
+});
+
+$(function(){
+
+     $('.radio_switcher li').click(function(){
+
+         var elem = '.' + $(this).data('type');
+         $('.data_form_opened').removeClass('data_form_opened');
+         $(elem).addClass('data_form_opened');
+     });
+});
+
+$(function(){
+
+     $('.accordeon_block dt').click(function(){
+
+            if($(this).parent().hasClass('opened')){
+
+               $(this).parent().removeClass('opened');
+            }else{
+
+              $(this).parent().addClass('opened');
+            }
+     });
+});
+
+$(function(){
+
+  $('.tabs_block .active_tab').click(function(){
+
+      if($(this).parents('.tabs_block').hasClass('tabs_block_opened')){
+
+            $(this).parents('.tabs_block').removeClass('tabs_block_opened');
+      }else{
+
+            $(this).parents('.tabs_block').addClass('tabs_block_opened');
+      }
+  });
+});
+/*
+                                         <div class="radio_group">
+                                             <input type="hidden" />
+                                             <span class="toggle_radio checked" data-value="idex">Idex</span>
+                                             <span class="toggle_radio" data-value="latoken">Latoken</span>
+                                             <span class="toggle_radio" data-value="etherdelta">EtherDelta</span>
+                                           </div><!-- radio_group  -->
+
+
+*/
