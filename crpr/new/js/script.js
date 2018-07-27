ec5be46cb7e66296eb4770c844225b95bfa1fa7c
Nikitina Victoria <vikitina@gmail.com>
2018-07-27 23:09:53 +0300

    crpr ne

---
+++ crpr/new/js/script.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+	
+	var winheight = $(window).height();
+	$('.first').css('height',winheight+'px');
+
+	$('.team_item').mouseover(function(){
+		$(this).addClass('team_item_hover');
+	});
+	$('.team_item').mouseout(function(){
+		$(this).removeClass('team_item_hover');	 
+	});	
+       $('.mob_btn').click(function(){
+           if( $('header').hasClass('opened_menu') ){
+                $('header').removeClass('opened_menu')
+            }else{
+                $('header').addClass('opened_menu')
+            }
+       });
+
+       $('.tabs_container  .tabs_menu  li').click(function(){
+
+            var tabcontainer_name = $(this).data('tabcontainer');
+            var tabcontainer_text = $(this).text();
+
+            $(this).closest('.tabs_menu').find('.selected').removeClass('selected');
+            $(this).addClass('selected');
+
+            var closest_level = $(this).closest('.tabs_container').find('.tabs_content');
+            $(closest_level[0]).children('.selected').removeClass('selected');
+
+            $(this).closest('.tabs_container').find('.tabs_content .'+tabcontainer_name).addClass('selected');
+
+            if($(this).closest('.tabs_container').find('span.mob_list')){
+                $(this).parent().parent().find('span.mob_list').text(tabcontainer_text);
+            }
+
+            
+       });
+
+      $(window).scroll(function(){
+        var winScrollTop = $(this).scrollTop();
+        if(winScrollTop > 0){
+           $('header').addClass('scrolled');
+         }
+           if(winScrollTop == 0){
+              $('header').removeClass('scrolled');
+         } 
+      });	
+});
