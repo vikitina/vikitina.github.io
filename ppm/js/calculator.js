4c6201d9ad6fb5b9eac62edada2971a6c7afde32
Nikitina Victoria <vikitina@gmail.com>
2018-08-22 17:18:45 +0300

    ppm

--- ppm/js/calculator.js
+++ ppm/js/calculator.js
@@ -1,9 +1,75 @@
+"use strict";
+
 $(document).ready(function(){
-    str = '';
-    data.map(function(elem, index){
-            str += ' ' + elem.name;
-            console.log(elem);
+    set_indexes_for_tree(data, null);
+
+    var str = '';
+
+    $.each( data, function( key, elem ) {
+        str += '<li class="choice_elem" data-address="'+ elem.node_address +'">' + elem.name + '</li>';
+      });
+    $('#first_data_block').html(str);
+});
+
+
+$(document).ready(function(){
+    
+
+    $('.calculator_block li[data-step="1"]').addClass('active');
+    $('.calculator_block li h5').mouseover(function(){
+        $('.calculator_block li.hover').removeClass('hover');
+        $(this).addClass('hover');
+    });
+
+    $('.calculator_block li h5').mouseout(function(){
+        $(this).removeClass('hover');
+       
+    });    
+    $('.nex_step_btn').click(function(){
+        if( $(this).parents('li').find('.control_element').val() ){
+            var parent_node = $(this).parents('li').find('.control_element').val();
+            $(this).parents('li').removeClass('active');
+            $(this).parents('li').next().addClass('active');
+            $(this).parents('li').next().find('.control_element').attr('data-parent', parent_node );
+            var node = get_node_by_index(data, parent_node);
+            
+            var str = "";
+            $.each( node.children, function( key, elem ) {
+                str += '<li class="choice_elem" data-address="'+ elem.node_address +'">' + elem.name + '</li>';
+            });
+            $(this).parents('li').next().find('.data_block .list_options').html(str);
+        }else{
+            $(this).parents('li').addClass('error');
+        }    
+    });
+    $('.calculator_block li .data_block').on('click','.choice_elem',function(){
+        var choiced_value = $(this).data('address');
+        $(this).closest('.data_block').find('.control_element').val(choiced_value);
 
     });
-    $('.obj').text(str);
+
 });
+/*------------------------------------------------------------------------------------------------*/
+function set_indexes_for_tree(tree, parent) {
+    $.each(tree, function(i, node) {
+
+        var node_address = parent !== null ? parent + "/" + i : i + "";
+        node.node_address = node_address;
+
+        if (node.children) {
+            set_indexes_for_tree(node.children, node_address);
+        };
+    });
+}
+
+// ------------------------------------------------
+function get_node_by_index(tree, index_str) {
+    var indexes = index_str.split(/\//);
+    var node = tree[indexes.shift()];
+
+    while (indexes.length) {
+        node = node.children[indexes.shift()];
+    }
+
+    return node;
+}
--- ppm/js/data.js
+++ ppm/js/data.js
@@ -1,32 +1,209 @@
 var data = [
     {
         name: 'Самоклеющаяся пленка',
-        characters: {}
+        children: [
+            {
+                name: 'Глянец',
+                children: [
+                    {
+                        name: '720 dpi',
+                        children: null
+                    },
+                    {
+                        name: '1440dpi',
+                        children: null
+                    },
+                    {
+                        name: 'УФ печать',
+                        children: null
+                    }
+                ]
+            },
+            {
+                name: 'Матовая',
+                children:  [
+                    {
+                        name: '720 dpi',
+                        children: null
+                    },
+                    {
+                        name: '1440dpi',
+                        children: null
+                    },
+                    {
+                        name: 'УФ печать',
+                        children: null
+                    }
+                ]
+            },
+            {
+                name: 'Прозрачная',
+                children:  [
+                    {
+                        name: '720 dpi',
+                        children: null
+                    },
+                    {
+                        name: '1440dpi',
+                        children: null
+                    },
+                    {
+                        name: 'УФ печать',
+                        children: null
+                    }
+                ]
+            },
+            {
+                name: 'Перфорированная',
+                children:  [
+                    {
+                        name: '720 dpi',
+                        children: null
+                    }
+                ]
+            }            
+        ]
     },
     {
         name: 'Баннер',
-        characters: {}
+        children: [
+            {
+                name: 'Литой',
+                children: null
+            },  
+            {
+                name: 'Ламинированный',
+                children: null
+            },   
+            {
+                name: 'Backlit',
+                children: null
+            },   
+            {
+                name: 'Mesh',
+                children: null
+            }                                          
+        ]
     },
     {
         name: 'Бумага',
-        characters: {}
+        children: [
+            {
+                name: 'Просветная "City"',
+                children: null
+            }, 
+            {
+                name: '"Blue Back"',
+                children: null
+            },                        
+        ]
     },
     {
         name: 'ПВХ',
-        characters: {}
-    },        
-    {
-        name: 'Композит',
-        characters: {}
+        children: [
+            {
+                name: 'Белый - 3 мм',
+                children: null
+            }, 
+            {
+                name: 'Белый - 4 мм',
+                children: null
+            },  
+            {
+                name: 'Белый - 5 мм',
+                children: null
+            }, 
+            {
+                name: 'Белый - 8 мм',
+                children: null
+            }                                  
+        ]
     },
     {
+        name: 'Композит',
+        children: [
+            {
+                name: 'Белый - 3 мм',
+                children: null
+            }                               
+        ]
+    }, 
+    {
         name: 'САН',
-        characters: {}
+        children: [
+            {
+                name: 'Белый - 3 мм',
+                children: null
+            }                               
+        ]
+    },  
+    {
+        name: 'Акрил',
+        children: [
+            {
+                name: 'Прозрачный - 1 мм',
+                children: null
+            }, 
+            {
+                name: 'Прозрачный - 2 мм',
+                children: null
+            },  
+            {
+                name: 'Молочный - 1 мм',
+                children: null
+            }, 
+            {
+                name: 'Молочный - 2 мм',
+                children: null
+            }, 
+            {
+                name: 'Молочный - 3 мм',
+                children: null
+            }                                    
+        ]
+    },   
+    {
+        name: 'Полистирол',
+        children: [
+            {
+                name: 'Белый - 0.75 мм',
+                children: null
+            }, 
+            {
+                name: 'Белый - 1 мм',
+                children: null
+            },  
+            {
+                name: 'Белый - 2 мм',
+                children: null
+            }, 
+            {
+                name: 'Белый - 3 мм',
+                children: null
+            }                                   
+        ]
     },
     {
         name: 'Полистирол',
-        characters: {}
-    }
-
+        children: [
+            {
+                name: 'Белый - 0.75 мм',
+                children: null
+            }, 
+            {
+                name: 'Белый - 1 мм',
+                children: null
+            },  
+            {
+                name: 'Белый - 2 мм',
+                children: null
+            }, 
+            {
+                name: 'Белый - 3 мм',
+                children: null
+            }                                   
+        ]
+    }                   
 ];
+
 
